63e97929f6c2037590379b0079595bb98c01bbfd
sorin <sorin@172.20.80.50>
2021-09-03 14:44:22 +0900

    first commit practice

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello World!");
+
+	  
+	}
+
+}
